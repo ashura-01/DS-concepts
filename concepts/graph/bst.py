6899b6b49a48250c22fcf235aaff4a4570de1b14
ashura <fmn.flame@gmail.com>
2025-10-17 18:37:57 +0600

    ss

---
+++ concepts/graph/bst.py
@@ -0,0 +1,183 @@
+from typing import Optional
+
+
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left: Optional[node] = None
+        self.right: Optional[node] = None
+
+
+root: Optional[node] = None
+
+
+def preOrder(temp: node):
+    if temp is None:
+        return
+    print(temp.data, end=" ")
+    preOrder(temp.left)
+    preOrder(temp.right)
+
+
+def inorder(temp: node):
+    if temp is None:
+        return
+    inorder(temp.left)
+    print(temp.data, end=" ")
+    inorder(temp.right)
+
+
+def postOrder(temp: node):
+    if temp is None:
+        return
+    postOrder(temp.left)
+    postOrder(temp.right)
+    print(temp.data, end=" ")
+
+
+def findMin(node: node):
+    while node and node.left:
+        node = node.left
+    return node
+
+
+def successor(target: node):
+    global root
+
+    if target is None:
+        return None
+    if target.right is not None:
+        return findMin(target.right)
+
+    succ: Optional[node] = None
+    curr = root
+
+    while curr is not None:
+        if target.data < curr.data:
+            succ = curr
+            curr = curr.left
+        elif target.data > curr.data:
+            curr = curr.right
+        else:
+            break
+    return succ
+
+
+def findMax(node: node):
+    while node and node.right:
+        node = node.right
+    return node
+
+
+def predecessor(target: node):
+    global root
+
+    if target is None:
+        return None
+    if target.left is not None:
+        return findMax(target.left)
+    pred: Optional[node] = None
+    curr = root
+
+    while curr is not None:
+        if target.data > curr.data:
+            pred = curr
+            curr = curr.right
+        elif target.data < curr.data:
+            curr = curr.left
+        else:
+            break
+    return pred
+
+
+def insert(nodes: node, value):
+    if nodes is None:
+        return node(value)
+    if value < nodes.data:
+        nodes.left = insert(nodes.left, value)
+    else:
+        nodes.right = insert(nodes.right, value)
+    return nodes
+
+
+def delete(target: int):
+    global root
+
+    """checking validity"""
+    if root is None:
+        return
+    curr = root
+    prev: Optional[node] = None
+
+    """searching..."""
+    while curr:
+        if curr.data == target:
+            break
+        prev = curr
+        if curr.data > target:
+            curr = curr.left
+        else:
+            curr = curr.right
+
+    if curr is None:
+        print("target not found...")
+        return
+
+    # has no child
+    if curr.left is None and curr.right is None:
+        if prev is None:
+            root = None
+        elif prev.left == curr:
+            prev.left = None
+        else:
+            prev.right = None
+    elif curr.left is None or curr.right is None:
+        child = curr.left if curr.left is not None else curr.right
+        if prev is None:
+            root = child
+        elif prev.left == curr:
+            prev.left = child
+        else:
+            prev.right = child
+    else:
+        prev = curr
+
+        succ = curr.right
+
+        while succ.left is not None:
+            prev = succ
+            succ = succ.left
+        curr.data = succ.data
+
+        if prev.left == succ:
+            prev.left = succ.right
+        else:
+            prev.right = succ.right
+
+
+def search(target: int):
+    global root
+
+    curr = root
+
+    while curr is not None:
+        if curr.data == target:
+            return curr
+        elif target < curr.data:
+            curr = curr.left
+        else:
+            curr = curr.right
+    return None
+
+
+if __name__ == "__main__":
+
+    arr = [12, 6, 9, 18, 4, 10, 5, 16, 49, 31, 43]
+
+    for values in arr:
+        root = insert(root, values)
+    inorder(root)
+
+    print()
+    data = successor(search(5))
+    print("successor of 5 is: ", data.data)
---
+++ concepts/graph/graph.py
@@ -0,0 +1,235 @@
+from collections import deque
+from enum import Enum
+from typing import List
+
+
+class colors(Enum):
+    WHITE = 0
+    GRAY = 1
+    BLACK = 2
+
+
+"""normal BFS"""
+
+
+def bfs(n, source, graph, result):
+
+    level = [-1] * n
+    que = deque()
+
+    level[source] = 0
+    que.append(source)
+    result.append(source)
+
+    while que:
+        current = que.popleft()
+        for neighbors in graph[current]:
+            if level[neighbors] == -1:
+                level[neighbors] = level[current] + 1
+                que.append(neighbors)
+                result.append(neighbors)
+
+
+"""bipartite"""
+
+
+def isBipartite(n, graph):
+    color = [-1] * n
+
+    for i in range(n):
+        if color[i] == -1:
+            que = deque()
+            color[i] = 0
+            que.append(i)
+
+        while que:
+            current = que.popleft()
+            for neighbors in graph[current]:
+                if color[neighbors] == -1:
+                    color[neighbors] = 1 - color[current]
+                    que.append(neighbors)
+                elif color[neighbors] == color[current]:
+                    return False
+    return True
+
+
+"""topological sort"""
+
+
+def toposort(n: int, graph):
+    indegree = [0] * n
+
+    for i in range(n):
+        for neigh in graph[i]:
+            indegree[neigh] += 1
+    que = deque()
+
+    for i in range(n):
+        if indegree[i] == 0:
+            que.append(i)
+    result = []
+
+    while que:
+        node = que.popleft()
+        result.append(node)
+
+        for neigh in graph[node]:
+            indegree[neigh] -= 1
+            if indegree[neigh] == 0:
+                que.append(neigh)
+    return result
+
+
+"""shortest path"""
+
+
+def shortest_bfs(n, source, target, graph):
+    visited = [False] * n
+    parent = [-1] * n
+    que = deque()
+
+    visited[source] = True
+    que.append(source)
+
+    while que:
+        current = que.popleft()
+
+        if current == target:
+            break
+        for neighbors in graph[current]:
+            if not visited[neighbors]:
+                visited[neighbors] = True
+                parent[neighbors] = current
+                que.append(neighbors)
+
+
+"""normal dfs"""
+
+
+def normalDFS(source: int, graph: list[List], color: List, result: List):
+    color[source] = colors.GRAY
+    result.append(source)
+
+    for neighbors in graph[source]:
+        if color[neighbors] == colors.WHITE:
+            normalDFS(neighbors, graph, color, result)
+    color[source] = colors.BLACK
+
+
+"""number of cycle dfs"""
+
+
+def cycleDFS(source: int, graph: List[List], color: List):
+    color[source] = colors.GRAY
+
+    for neighbor in graph[source]:
+        if color[neighbor] == colors.WHITE:
+            if cycleDFS(neighbor, graph, color):
+                return True
+        elif color[neighbor] == colors.GRAY:
+            return True
+    color[source] = colors.BLACK
+
+    return False
+
+
+def numberOfCycle(n, graph, color):
+    count = 0
+    for i in range(n):
+        if color[i] == colors.WHITE:
+
+            if cycleDFS(i, graph, color):
+                count += 1
+    return count
+
+
+"""printing circular path"""
+
+
+def cyclePath(source, graph: List[List], color: List, path: List, cycles):
+    color[source] = colors.GRAY
+    path.append(source)
+
+    for neighbors in graph[source]:
+        if color[neighbors] == colors.WHITE:
+            cyclePath(neighbors, graph, color, path, cycles)
+
+        elif color[neighbors] == colors.GRAY:
+            cycle = []
+            flag = False
+            for nodes in path:
+                if nodes == neighbors:
+                    flag = True
+                if flag:
+                    cycle.append(nodes)
+            cycle.append(neighbors)
+            cycles.append(cycle)
+    color[source] = colors.BLACK
+    path.pop()
+
+
+def printCycle(n, graph):
+    color = [colors.WHITE] * n
+    path = []
+    cycles = []
+
+    for i in range(n):
+        if color[i] == colors.WHITE:
+            cyclePath(i, graph, color, path, cycles)
+    i = 1
+    for numbers in cycles:
+        print("cycle ", i, ":", numbers)
+        i += 1
+
+
+"""all path from source to destination"""
+
+
+def allpath_dfs(source, target, graph, result, storage):
+
+    storage.append(source)
+
+    if source == target:
+        result.append(storage[:])
+    else:
+        for neighbors in graph[source]:
+            allpath_dfs(neighbors, target, graph, result, storage)
+    storage.pop()
+
+
+def allPathPrinting(n, source, target, graph):
+
+    result = []
+    storage = []
+    allpath_dfs(source, target, graph, result, storage)
+    i = 1
+    for numbers in result:
+        print("cycle ", i, ":", numbers)
+        i += 1
+
+
+"""main function"""
+
+if __name__ == "__main__":
+    n = 6
+    graph = [[] for _ in range(n)]
+
+    graph[0] = [1, 2]
+    graph[3] = [5]
+    graph[2] = [4]
+    graph[1] = [3, 4]
+    graph[4] = [2, 5]
+    graph[5] = [3]
+
+    color = [colors.WHITE] * n
+    result = []
+
+    normalDFS(0, graph, color, result)
+
+    print(result)
+    color = [colors.WHITE] * n
+    print(f"the number of cycle {numberOfCycle(n,graph,color)}")
+
+    printCycle(n, graph)
+
+    allPathPrinting(n, 0, 4, graph)
